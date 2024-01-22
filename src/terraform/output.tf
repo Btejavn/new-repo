3be4b00e65010f4a503e35f2fe9fa920a47516f8
Btejavn <152203328+Btejavn@users.noreply.github.com>
2024-01-22 11:04:41 -0500

    Update output.tf

---
+++ src/terraform/output.tf
@@ -0,0 +1,8 @@
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = var.resource_group_name
+}
+output "vnet_name" {
+  description = "The name of the vnet"
+  value       = var.vnet_name
+}
